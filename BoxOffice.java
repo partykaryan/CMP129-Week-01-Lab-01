44a38deaa12dea37cb99254a51432926d345faef
partyka.ryan <partyka.ryan@student.ccm.edu>
2026-09-01 16:56:50 -0400

    completed lab1

--- BoxOffice.java
+++ BoxOffice.java
@@ -1,4 +1,42 @@
 public class BoxOffice {
+public static void main(String[] args) {
+
+Scanner keyboard = new Scanner(System.in);
+
+//Variable Initialization & Declaration 
+String movieName;
+int adultTickets;
+int childTickets;
+final double adultPrice = 10.00;
+final double childPrice = 6.00;
+double grossProfit;
+final double distributorRate = 0.80;
+double distributorAmount;
+double netProfit;
+
+//User Prompts
+System.out.println("What is the name of the movie ==> ");
+movieName = keyboard.nextline();
+
+System.out.println("How many adult tickets were sold ==> ");
+adultTickets = keyboard.nextInt();
     
-    
-}
+System.out.println("How many child tickets were sold ==> ");
+childTickets = keyboard.nextInt();
+
+
+//Calculations
+grossProfit = (adultTickets * adultPrice) + (childTickets * childPrice);
+distributorAmount = (grossProfit * distributorRate);
+netProfit = (grossProfit - distributorAmount);
+
+//Output Statements
+System.out.println("Movie Name: " + movieName);
+System.out.println("Adult Tickets Sold: " + adultTickets);
+System.out.println("Child Tickets Sold: " + childTickets);
+System.out.println("Gross Box Office Profit: " + grossProfit/%.2f);
+System.out.println("Net Box Office Profit:  " + netProfit);
+System.out.println("Amount Paid to Distributor: " + distributorAmount);
+
+}//end of main method
+}//end of public class
